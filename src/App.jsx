ebe643cdf1cacec0ad0c491d1717106bd6abfa39
Oskar <proton2006tom@gmail.com>
2025-11-19 06:19:12 +0200

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import 'bulma/css/bulma.css';
 import './App.scss';
 
@@ -14,33 +15,74 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App = () => (
-  <div className="section content">
-    <div className="buttons">
-      <button type="button" className="button is-info is-light">
-        Sort alphabetically
-      </button>
-
-      <button type="button" className="button is-success is-light">
-        Sort by length
-      </button>
-
-      <button type="button" className="button is-warning is-light">
-        Reverse
-      </button>
-
-      <button type="button" className="button is-danger is-light">
-        Reset
-      </button>
-    </div>
-
-    <ul>
-      <li data-cy="Good">Dumplings</li>
-      <li data-cy="Good">Carrot</li>
-      <li data-cy="Good">Eggs</li>
-      <li data-cy="Good">Ice cream</li>
-      <li data-cy="Good">Apple</li>
-      <li data-cy="Good">...</li>
-    </ul>
-  </div>
+export const GoodList = ({ goods }) => (
+  <ul className="GoodList">
+    {goods.map(good => (
+      <li key={good} data-cy="Good">
+        {good}
+      </li>
+    ))}
+  </ul>
 );
+
+export const App = () => {
+  const [sortField, setSortField] = useState(goodsFromServer);
+
+  function sortAlphabetically() {
+    const sortedGoods = [...sortField].sort();
+
+    setSortField(sortedGoods);
+  }
+
+  function sortLenghtly() {
+    const sortedGoods = [...sortField].sort(
+      (good1, good2) => good1.length - good2.length,
+    );
+
+    setSortField(sortedGoods);
+  }
+
+  function reverseList() {
+    setSortField([...sortField].reverse());
+  }
+
+  return (
+    <div className="section content">
+      <div className="buttons">
+        <button
+          type="button"
+          className="button is-info is-light"
+          onClick={() => sortAlphabetically()}
+        >
+          Sort alphabetically
+        </button>
+
+        <button
+          type="button"
+          className="button is-success is-light"
+          onClick={() => sortLenghtly()}
+        >
+          Sort by length
+        </button>
+
+        <button
+          type="button"
+          className="button is-warning is-light"
+          onClick={reverseList}
+        >
+          Reverse
+        </button>
+
+        <button
+          type="button"
+          className="button is-danger is-light"
+          onClick={() => setSortField(goodsFromServer)}
+        >
+          Reset
+        </button>
+      </div>
+
+      <GoodList goods={sortField} />
+    </div>
+  );
+};
